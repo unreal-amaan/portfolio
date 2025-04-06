31c64a60bf2e9bd3b132be425340bc87fa5248d4
UNREAL-AMAAN <amaanuddinsyed2005@gmail.com>
2025-04-06 23:41:39 +0530

    minor changes

--- src/Resume/resume.tsx
+++ src/Resume/resume.tsx
@@ -1,8 +1,151 @@
+//icons
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs } from "react-icons/fa";
+import {
+    SiTailwindcss,
+    SiExpress,
+    SiMongodb,
+    SiPostgresql,
+    SiPostman,
+    SiTypescript,
+} from "react-icons/si";
+import { TbBrandCpp } from "react-icons/tb";
+
+const about = {
+    title: "About Me",
+    description:
+        "This is Amaan, B.Tech Undergrad from Hyderabad. I love to code scalable backend systems",
+    fields: [
+        {
+            title: "Name",
+            value: "Syed Amaanuddin",
+        },
+        {
+            title: "Experience",
+            value: "Fresher",
+        },
+        {
+            title: "Nationality",
+            value: "Indian",
+        },
+        {
+            title: "Email",
+            value: "amaanuddinsyed2005@gmail.com",
+        },
+        {
+            title: "Languages",
+            value: "English, Urdu",
+        },
+    ],
+};
+
+const education = {
+    title: "My Education",
+    description:
+        "Currently in 2nd year of B.Tech in Computer Science and Engineering at GRIET Hyderabad.",
+    items: [
+        {
+            institution: "GRIET Hyderabad",
+            degree: "B.Tech in Computer Science and Engineering",
+            period: "2023 - Present",
+        },
+        {
+            institution: "Shree Akshaya Junior College",
+            degree: "Intermediate in PCM",
+            period: "2021 - 2023",
+        },
+        {
+            institution: "Kothwal high school",
+            degree: "High School",
+            period: "2008 - 2021",
+        },
+    ],
+};
+
+const skills = {
+    title: "My Skills",
+    description: "I have knowledge of the following technologies",
+    items: [
+        {
+            icon: <FaHtml5 />,
+            title: "HTML",
+        },
+        {
+            icon: <FaCss3Alt />,
+            title: "CSS",
+        },
+        {
+            icon: <FaJs />,
+            title: "JavaScript",
+        },
+        {
+            icon: <SiTypescript />,
+            title: "TypeScript",
+        },
+        {
+            icon: <TbBrandCpp />,
+            title: "C++",
+        },
+        {
+            icon: <FaReact />,
+            title: "React.js",
+        },
+        {
+            icon: <FaNodeJs />,
+            title: "Node.js",
+        },
+        {
+            icon: <SiExpress />,
+            title: "express.js",
+        },
+        {
+            icon: <SiTailwindcss />,
+            title: "tailwindcss",
+        },
+        {
+            icon: <SiMongodb />,
+            title: "MongoDB",
+        },
+        {
+            icon: <SiPostgresql />,
+            title: "PostgreSQL",
+        },
+        {
+            icon: <SiPostman />,
+            title: "Postman",
+        },
+    ],
+};
+
+//components
+import {
+    Tabs,
+    TabsContent,
+    TabsTrigger,
+    TabsList,
+} from "../components/ui/tabs";
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+
 
 const Resume = () => {
-  return (
-    <div>Resume</div>
-  )
-}
+    return <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1, duration: 1, ease: "easeInOut" }}
+      className="min-h-[80vh] flex items-center justify-center py-12 lg:py-0"
+    >
+      <div className="container mx-auto mx-auto w-11/12 md:w-5/6 lg:w-3/4">
+        <Tabs>
+          <TabsList>
+            <TabsTrigger>Education</TabsTrigger>
+            <TabsTrigger>Skills</TabsTrigger>
+            <TabsTrigger>About me</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
+    </motion.div>;
+};
 
-export default Resume
+export default Resume;
--- src/header/Mobilenav.tsx
+++ src/header/Mobilenav.tsx
@@ -1,59 +1,68 @@
 import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
 import { CiMenuFries } from "react-icons/ci";
-
+import { useState } from "react";
 
 //components
 import { Sheet, SheetContent, SheetTrigger } from "../components/ui/sheet";
 
 const links = [
-    {
-        name: "Home",
-        path: "/home",
-    },
-    {
-        name: "Work",
-        path: "/work",
-    },
-    {
-        name: "Resume",
-        path: "/resume",
-    },
-    {
-        name: "Contact",
-        path: "/contact",
-    },
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Work",
+    path: "/work",
+  },
+  {
+    name: "Resume",
+    path: "/resume",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
 ];
 
-const hovereffect = "text-xl hover:text-primary hover:transition-all duration-500"
-
+const hovereffect = "text-xl hover:text-primary hover:transition-all duration-500";
 
 const Mobilenav = () => {
-    const pathname = useLocation().pathname;
+  const pathname = useLocation().pathname;
+  const [open, setOpen] = useState(false);
 
-    return (
-        <Sheet>
-            <SheetTrigger className="flex justify-center items-center">
-                <CiMenuFries className="text-3xl text-primary" />
-            </SheetTrigger>
-            <SheetContent className="flex flex-col">
-                <div className="mt-32 mb-40 text-center text-2xl">
-                    <Link to="/">
-                        <h1 className="text-4xl font-semibold">
-                            Syed <span className="text-primary">.</span>
-                        </h1>
-                    </Link>
-                </div>
-                <nav className="flex flex-col gap-8 place-items-center text-xl">
-                    {links.map((link) => (
-                        <Link to={link.path} className={`${hovereffect} ${link.path === pathname && "text-primary border-b-2"}`}>
-                            {link.name}
-                        </Link>
-                    ))}
-                </nav>
-            </SheetContent>
-        </Sheet>
-    );
+  const toggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-3xl text-primary" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link to="/" onClick={() => setOpen(false)}>
+            <h1 className="text-4xl font-semibold">
+              Syed <span className="text-primary">.</span>
+            </h1>
+          </Link>
+        </div>
+        <nav className="flex flex-col gap-8 place-items-center text-xl">
+          {links.map((link) => (
+            <Link
+              onClick={() => setOpen(false)}
+              to={link.path}
+              key={link.name}
+              className={`${hovereffect} ${link.path === pathname ? "text-primary border-b-2" : ""}`}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default Mobilenav;
